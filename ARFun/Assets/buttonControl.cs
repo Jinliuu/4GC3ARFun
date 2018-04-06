8cc42f1eb3630e57b45ba3b9abcb725f4eaf5b84
Jinn <liuriedel@gmail.com>
2018-04-06 19:54:42 -0400

    update

---
+++ ARFun/Assets/buttonControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonControl : MonoBehaviour {
+
+    public GameObject currentTarget;
+    public GameObject currentTarget2;
+    Animator currentAnimator;
+    Animator currentAnimator2;
+    Animator myAnimator;
+	// Use this for initialization
+	void Start () {
+        currentAnimator = currentTarget.GetComponent<Animator>();
+        currentAnimator2 = currentTarget2.GetComponent<Animator>();
+        myAnimator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void onButtonClick()
+    {
+        myAnimator.SetTrigger("onClick");
+        currentAnimator.SetBool("Next",true);
+        currentAnimator2.SetBool("Next", true);
+    }
+
+    public void onStopClick()
+    {
+        myAnimator.SetTrigger("onClick");
+        currentAnimator.SetBool("Next", false);
+        currentAnimator2.SetBool("Next", false);
+    }
+
+    public void onEnterEvent()
+    {
+        myAnimator.SetBool("onEnter", true);
+    }
+
+    public void onExitEvent()
+    {
+        myAnimator.SetBool("onEnter", false);
+    }
+}
